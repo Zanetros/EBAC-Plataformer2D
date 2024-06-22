e2d809df39a16ac5c62a4c23cfda3d92829c0e1c
Zanetros <73186724+Zanetros@users.noreply.github.com>
2024-06-22 17:01:28 -0300

    commit

---
+++ TravestiInrrustido/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void Load(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Load(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
